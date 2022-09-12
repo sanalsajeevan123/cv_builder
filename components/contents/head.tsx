f896e8c28f9d7b71d3af6bfb2c1c8d588a1ad904
sanalsajeevan123 <sanalsajeevan123@gmail.com>
2022-09-12 17:50:53 +0530

    update

---
+++ components/contents/head.tsx
@@ -0,0 +1,8 @@
+const Head=()=>{
+    return(
+        <div className="w-full">
+            <h1 className="">Head Component</h1>
+        </div>
+    )
+}
+export default Head
---
+++ tailwind.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  content: ['./pages/**/*.js', './components/**/*.js','./pages/**/*.tsx', './components/**/*.tsx'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
